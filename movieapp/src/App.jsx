60ec3dd5b6b46c79a64c98f01d6692c67f2ebb5e
Bavishyaa <rbavishyaa@gmail.com>
2025-06-08 17:40:19 +1000

    Initial commit

---
+++ movieapp/src/App.jsx
@@ -0,0 +1,23 @@
+import './css/App.css'
+import FavouritesPage from './pages/Favourites'
+import HomePage from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext'
+
+function App() {
+
+  return (
+    <MovieProvider>
+      <NavBar/>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/favourites" element={<FavouritesPage/>}/>
+        </Routes>
+      </main>
+    </MovieProvider>
+  )
+}
+
+export default App
